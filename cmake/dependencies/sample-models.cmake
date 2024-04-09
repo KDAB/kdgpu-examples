29f8e231f30c36c54cc98a19c994a54b585a6577
Joshua Goins <joshua.goins@kdab.com>
2024-04-09 10:17:39 -0400

    Change the URL to the sample model to public GitHub
    Change-Id: I74e2d6337bec68fb7d026a5769ee259ec390083a
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu-examples/+/140366
    Reviewed-by: Joshua Benjamin Goins <joshua.goins@kdab.com>

--- cmake/dependencies/sample-models.cmake
+++ cmake/dependencies/sample-models.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
     sample-models
-    GIT_REPOSITORY ssh://codereview.kdab.com:29418/kdab/kdgpu-examples-data
+    GIT_REPOSITORY git@github.com:KDAB/kdgpu-examples-data.git
     GIT_TAG master
 )
 FetchContent_MakeAvailable(sample-models)
